18050f1670f3f423e8e368979731dacaba4afa3b
Chad Campbell <cwc5189@rit.edu>
2015-04-02 00:52:04 -0700

    Added fixes for twitter
    Fixed two scenarios:
    1. "everloading" timeline
    2. changing pages on twitter didn't throw onload event, so now I check
    href every half a second

--- background.js
+++ background.js
@@ -1,25 +1,52 @@
-window.onload=function()
-{
-	if(document.location.href.indexOf("twitter.com") >= 0){
-				
+function onWindowOnLoad()
+{		
+	if(document.location.href.indexOf("twitter.com") >= 0){		
 		// For statuses there are no previews, so we don't need the listeners and can just
-		// find the video iframe because it is already loaded
+		// find the video iframe because it is already loaded	
+		console.log("On twitter");
+		
 		if(document.location.href.indexOf("status") >= 0)
+		{
+			console.log("Opened twitter status");
 			enableFullScreenTwitter();
+		}
 		else
+		{
+			console.log("addingListeners");
 			addTwitterListeners();
+			addTwitterFeedListener();
+		}
 		
 	}
 	
 	if(document.location.href.indexOf("youtube.com") < 0) {
 		enableFullScreenGeneric();
 	}
+	
+	locChangeHandler();
 
+}
+
+//window.onload=onWindowOnLoad;
+window.addEventListener("load", onWindowOnLoad);
+
+function locChangeHandler(){
+    this.oldLoc = window.location.href;
+    this.Check;
+
+    var that = this;
+    var detect = function(){
+        if(that.oldLoc!=window.location.href){
+            that.oldLoc = window.location.href;
+			onWindowOnLoad();
+        }
+    };
+    this.Check = setInterval(function(){ detect() }, 500);
 }
 
 function enableFullScreenGeneric(){
 
-	iframes = document.getElementsByTagName("iframe");
+	var iframes = document.getElementsByTagName("iframe");
 	
 	// Find iframes with src of youtube
 	for(var i = 0; i < iframes.length; i++){
@@ -56,20 +83,20 @@ function enableFullScreenGeneric(){
 
 function enableFullScreenTwitter(){
 	
-	iframes = document.getElementsByTagName("iframe");
+	var iframes = document.getElementsByTagName("iframe");
 	
 	//console.log("enableFullScreenTwitter: Find twitter player cards");
-	for(i = 0; i < iframes.length; i++){
+	for(var i = 0; i < iframes.length; i++){
 		if(iframes[i].src.indexOf("cardname=player") >= 0){
 			video = iframes[i];
-			//console.log("Found twitter player card");				
+			console.log("Found twitter player card");				
 			
 			//Get the tweet container's first link, twitter only looks at the first one for media
 			tweetLinks = iframes[i].parentNode.parentNode.parentNode.parentNode.parentNode.getElementsByClassName("twitter-timeline-link");
 			
 			//console.log(tweetLinks);
 
-			for(i = 0; i < tweetLinks.length; i++){
+			for(var i = 0; i < tweetLinks.length; i++){
 				mediaLink = tweetLinks[i];
 			
 				// If the media link is youtube, do the thing!
@@ -80,10 +107,10 @@ function enableFullScreenTwitter(){
 					videoUrl = mediaLink.attributes["data-expanded-url"].value.replace("youtu.be", "youtube.com").replace("youtube.com", "youtube.com/embed/").replace("http://", "https://");
 					
 					// Then add our full screen, autoplay, and autohide to it.
-					video.src = videoUrl + "?fs=1&autoplay=1&autohide=1";
+					video.src = videoUrl + "?fs=1&autohide=1";
 					
 					// Have to wait because it wouldn't set the height until it was finished changing src
-					setTimeout(function(){video.height = 315;}, 200);
+					setTimeout(function(){video.height = 315;}, 500);
 				}
 			}
 		}
@@ -91,12 +118,14 @@ function enableFullScreenTwitter(){
 }
 
 // Called when a twitter iframe card class object is modified. See addTwitterListeners()
-function enableFullScreenTwitterListener(){
+function enableFullScreenTwitterListener(target){
 	
-	iframes = document.getElementsByTagName("iframe");
+	var iframes = target.getElementsByTagName("iframe");
+	
+	console.log("State changed on twitter player card! " + target);
 	
 	//.log("Find twitter player cards");
-	for(i = 0; i < iframes.length; i++){
+	for(var i = 0; i < iframes.length; i++){
 		if(iframes[i].src.indexOf("cardname=player") >= 0){
 			//console.log("Found twitter player card");				
 			
@@ -120,12 +149,38 @@ function enableFullScreenTwitterListener(){
 function addTwitterListeners(){
 	// select the twitter cards that are iframe containers (dependant on twitter implementation of classes)
 	var target = document.querySelector('.js-macaw-cards-iframe-container');
+
+	console.log("Found " + document.getElementsByClassName('js-macaw-cards-iframe-container').length + " player cards!");
+	console.log(document.getElementsByClassName('js-macaw-cards-iframe-container'));
+	// create an observer instance that calls our twitter specific fullscreen enabler
+	// when something changes
+	var observer = new MutationObserver(function(mutations) {
+	  mutations.forEach(function(mutation) {
+		enableFullScreenTwitterListener(mutation.target);
+	  });    
+	});
+	 
+	// configuration of the observer:
+	var config = { attributes: true, childList: true, characterData: true };
+
+	// Actually tell the observer instance to watch the specified objects
+	for(var i = 0; i < document.getElementsByClassName('js-macaw-cards-iframe-container').length; i++)
+	{
+		observer.observe(document.getElementsByClassName('js-macaw-cards-iframe-container')[i], config);
+	}
+
+
+}
+
+function addTwitterFeedListener(){
+	var target = document.querySelector('.GridTimeline');
 	 
 	// create an observer instance that calls our twitter specific fullscreen enabler
 	// when something changes
 	var observer = new MutationObserver(function(mutations) {
 	  mutations.forEach(function(mutation) {
-		enableFullScreenTwitterListener();
+		addTwitterListeners();
+		console.log("Detected twitter feed update");
 	  });    
 	});
 	 
